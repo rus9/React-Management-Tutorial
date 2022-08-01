a66056383360b956c166ff8809b87651590f724a
rus <ru909@naver.com>
2022-08-01 17:46:52 +0900

    Create Customer js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+  'id': '1',
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '어우동',
+  'birthday': '9218594',
+  'gender': '남자',
+  'job': '대학생',
+},
+{
+  'id': '2',
+  'image': 'https://placeimg.com/64/64/2y',
+  'name': '기라라',
+  'birthday': '42448594',
+  'gender': '여자',
+  'job': '여직생',
+},
+{
+  'id': '1',
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '너비아니',
+  'birthday': '548594',
+  'gender': '남자',
+  'job': '놈팽이',
+}
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logd" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customers.map(c => {return (
+            <Customer 
+             key={c.id}
+             id={c.id}
+             image={c.image}
+             name={c.name}
+             birthday={c.birthday}
+             gender={c.gender}
+             job={c.job}
+            />
+            );
+          })
+        }
       </div>
+      
     );
   }
 }
